c31a17717e46a3efb2e7c60b1f041e2943c8c15b
Meteor337 <kometa1881@gmail.com>
2026-03-29 12:39:17 +0300

    Изменен конфиг fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fastfetch
     
-    # Функция приглашения с зеленой стрелкой ➜
+    # Функция приглашения с путём на первой строке и ➜ на второй
     function fish_prompt
         # Получаем имя пользователя и хост
         set -l user (whoami)
@@ -12,9 +12,15 @@ if status is-interactive
         # Сокращаем путь (заменяет /home/username на ~)
         set -l pwd (prompt_pwd)
         
-        # Выводим приглашение: user@host путь ➜
-        echo -n -s "$user" "@" "$host" " " "$pwd" " " (set_color green) "➜" (set_color normal) " "
+        # Первая строка: user@host и путь с цветами
+        # Зелёный цвет для пользователя, голубой для хоста
+        echo -n -s (set_color green) "$user" (set_color normal) "@" (set_color cyan) "$host" (set_color normal) " " (set_color yellow) "$pwd" (set_color normal)
+        echo  # Пустой echo для переноса строки
+        
+        # Вторая строка: зелёная стрелка для ввода команд
+        echo -n -s (set_color green) "➜" (set_color normal) " "
     end
+    
     function y
         set tmp (mktemp -t "yazi-cwd.XXXXXX")
         command yazi $argv --cwd-file="$tmp"
